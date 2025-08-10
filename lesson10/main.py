0979050c61440c5a928c7bf971152cf89d0ce8ae
vnicolev <vkunicolev@gmail.com>
2025-08-10 08:20:46 +0000

    Add streamlit to establish localhost

--- lesson10/main.py
+++ lesson10/main.py
@@ -6,12 +6,15 @@
 這是一個簡單的命令列介面程式，用於查詢台鐵車站資訊和進出站人數
 """
 import tool
+import streamlit as st
+
+# establish localhost: `streamlit run main.py`
 
 def main():
     """主程式"""
+    st.title("台鐵車站名稱列表")
     result = tool.get_station_name()
-    for station in result:
-        print(station)
+    st.dataframe(result, width=400, height=600)
 
 if __name__ == "__main__":
     main()
